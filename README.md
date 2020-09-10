ec707abee0a6276ef508fadce436364c80fa49bd
gamalasker <gamalasker@gmail.com>
2020-09-10 16:56:29 +0200

    111

--- README.md
+++
@@ -1,5 +0,0 @@
-# commons
-
-Don't use this library for commercial purposes, because it's used as a collection of code
-
-Want to use it in your app? Send an email to "infos@gamalasker.com" for permission
--- commons/src/main/java/com/gamalasker/commons/MethodCollection.java
+++ commons/src/main/java/com/gamalasker/commons/MethodCollection.java
@@ -1,15 +1,23 @@
 package com.gamalasker.commons;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.util.Log;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 
 public class MethodCollection {
 
-    public static boolean isAppHacked(String PACKAGE_NAME, Context mContext, String googlePackageName, String amazonPackageName) {
+    public static boolean isAppHa556Fg65d(String PACKAGE_NAME, Context mContext, String googlePackageName, String amazonPackageName, String signature) throws PackageManager.NameNotFoundException {
 
         String installer = mContext.getPackageManager().getInstallerPackageName(PACKAGE_NAME);
 
+        @SuppressLint("PackageManagerGetSignatures")
+        Signature[] signatures = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), PackageManager.GET_SIGNATURES).signatures;
+
         if (mContext.getPackageName().startsWith("com.gamalasker")) {
+            return false;
+        } else if (installer != null && installer.startsWith(googlePackageName) && installer.startsWith(amazonPackageName)) {
             return false;
         } else if (installer == null) {
             return true; // BOOM!
@@ -17,9 +25,12 @@ public class MethodCollection {
             return true; // BOOM!
         } else if (mContext.getPackageName().compareTo(PACKAGE_NAME) != 0) {
             return true; // BOOM!
+        } else if (!signatures[0].toCharsString().equals(signature)){
+            return true; // BOOM!
         }
 
         return false;
     }
+
 
 }
